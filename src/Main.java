2e6f3cb76dfa5fbca14e17eded5175607a4d1831
sasha-vovc <vovcenco.sasha.andrei@gmail.com>
2024-11-15 22:56:39 +0200

    Finished quickstart project

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ import main.Init;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1");
         Init.instance().startAndWait(true);
-        System.out.println("2");
+
     }
 }
